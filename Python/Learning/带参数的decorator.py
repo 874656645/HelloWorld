b44986fccd96966e3e32c99a71d274228dc18d09
Zhan Cui <cui_zhan@hotmail.com>
2018-02-22 22:30:25 +0800

    带参数的装饰器

---
+++ Python/Learning/带参数的decorator.py
@@ -0,0 +1,25 @@
+import time
+
+def performance(unit):
+    def per_decorator(f):
+        t1 = time.time()        
+        def wrapper(*args, **kwargs):
+            f(*args, **kwargs)
+            t2 = time.time()
+            t = 0
+            if unit == 'ms':
+                t = (t2 - t1) * 1000  
+            else:
+                t = (t2 - t1)
+            # t = (t2 - t1) * 1000 if unit == 'ms' else (t2 - t1)
+            print('call %s() in %f %s' %(f.__name__, t, unit))
+            return f(*args, **kwargs)
+        return wrapper
+    return per_decorator
+
+@performance('ms')
+def factorial(n):
+    from functools import reduce
+    return reduce(lambda x,y: x*y, range(1, n+1))
+
+print(factorial(20))
