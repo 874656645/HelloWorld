5dc507f8a56a6bc75ffd68291a30dc71d53e928b
Zhan Cui <cui_zhan@hotmail.com>
2018-04-22 22:25:29 +0800

    更新C++学习

--- C++/Learning/HelloWorld.cpp
+++ C++/Learning/HelloWorld.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
 using namespace std;
+
+typedef struct X
+{
+    char a;     // 1 byte，7 padding bytes
+    double b;   // 8 bytes
+    int c;      // 4 bytes， 4 padding bytes
+}x;
+
+void change(int* a, int &b, int c)
+{
+    c = *a;
+    b = 3;
+    *a = 2;
+}
+
 int main()
 {
-    int a;
-    cout << "HelloWorld!" << endl;
-    cout << (char)65 << endl;
-    cin >> a;
+    int d[] = {1,2,3,4,6};
+    int *ptr;
+    ptr = (int*)(&d + 1);   // 数组名和数组名取地址的区别
+    printf("%5d%5d\n", *(d + 1), *(ptr - 1));
+
+    int a = 1, b = 2, c = 3;
+    change(&a, b, c);
+    printf("%5d%5d%5d\n", a, b, c);
+    printf("%5d%5d%5d\n", sizeof(d), sizeof(d[1]), sizeof(X));
+    
+    // printf("Hello World");
+
     return 0;
 }
