3fdf5c1c2557b590a94d1989620a45f8f572a5e6
Zhan Cui <cui_zhan@hotmail.com>
2018-06-13 22:23:34 +0800

    wstring 转utf-8编码

--- C++/Learning/WritingUTF8.cpp
+++ C++/Learning/WritingUTF8.cpp
@@ -1,13 +1,59 @@
 #include<windows.h>
+#include<iostream>
+#include<string>
+#include<fstream>
 
-std::string to_utf8(const wchar_t* buffer, int len)
+using namespace std;
+
+string to_utf8(const wchar_t* buffer, int len)
 {
-    int nChars = ::WideCharToMultiByte(CP_UTF8, 0, buffer, len, NULL, 0, NULL, NULL);
+    int nChars = ::WideCharToMultiByte(
+        CP_UTF8, 
+        0, 
+        buffer, 
+        len, 
+        NULL, 
+        0, 
+        NULL, 
+        NULL);
     
     if (nChars == 0) {
         return "";
     }
 
     string newBuffer;
-    
+    newBuffer.resize(nChars);
+    ::WideCharToMultiByte(
+        CP_UTF8, 
+        0, 
+        buffer, 
+        len, 
+        const_cast<char*>(newBuffer.c_str()),
+         nChars, 
+         NULL,
+         NULL);
+
+    return newBuffer;
+}
+
+string to_utf8(const wstring & str)
+{
+    return to_utf8(str.c_str(), (int)str.size());
+}
+
+int main(int argc, char const *argv[])
+{
+    ofstream outfile;
+    outfile.open("D://test.xml", ios::out | ios::binary);
+    wstring text =
+                L"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                L"<root description=\"this is a naïve example\">\n</root>";
+
+    string outtext = to_utf8(text);
+
+    outfile << outtext;
+
+    outfile.close();
+
+    return 0;
 }
